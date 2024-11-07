ba7d75f9502ded17e93c190ce693305e8976a617
pulseneon <thebooom@yandex.ru>
2024-11-07 22:54:31 +0300

    Разделение сервиса шифрования и логики энкриптера

--- Mailgram.Server/Services/Interfaces/IEncryptService.cs
+++ Mailgram.Server/Services/Interfaces/IEncryptService.cs
@@ -1,11 +1,9 @@
-﻿using System.Security.Cryptography;
-
-namespace Mailgram.Server.Services.Interfaces;
+﻿namespace Mailgram.Server.Services.Interfaces;
 
 public interface IEncryptService
 {
-    byte[] Encrypt(byte[] data, byte[] key, byte[] iv);
-    byte[] Decrypt(byte[] data, byte[] key, byte[] iv);
-    byte[] SignFile(byte[] fileData, RSAParameters privateKey);
-    bool VerifyFileSign(byte[] fileData, byte[] signature, string publicKeyXml);
+    void EncryptFile(string filepath);
+    string DecryptFile(string filepath);
+    string CreateMessageSign();
+    bool VerifySign();
 }
